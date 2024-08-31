1a925250a370600dd0e41c2f5ce8f62c45cb01c2
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2024-08-31 23:50:08 +0530

    working on alerts

--- client/src/pages/ShowAlarms/ShowAlarms.jsx
+++ client/src/pages/ShowAlarms/ShowAlarms.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import "./ShowAlarms.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import { useParams,Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import AlarmModal from "./AlarmModal";
 import { server_url } from "../../constants/constants";
-import axiosInstance from "../../helpers/axios/axiosInstance"; 
+import axiosInstance from "../../helpers/axios/axiosInstance";
 import { BsTrash, BsPencilSquare, BsKey } from "react-icons/bs";
 import EditAlarmModal from "./EditAlarmModal";
 import DoctorAlarmModal from "./DoctorAlarmModal";
@@ -37,16 +37,18 @@ function ShowAlarms() {
     setShowEditModal(false);
   };
   const deleteAlarm = async (id) => {
-    const isConfirmed = window.confirm("Are you sure you want to delete this alarm?");
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete this alarm?"
+    );
     if (isConfirmed) {
       try {
         console.log(id);
         const result = await axiosInstance.delete(`${server_url}/alarms/${id}`);
-      
+
         console.log("Response:", result.data);
         // Update the UI by filtering out the deleted alarm
-        setUserAlarmData(prevData =>
-          prevData.filter(alarm => alarm.id !== id)
+        setUserAlarmData((prevData) =>
+          prevData.filter((alarm) => alarm.id !== id)
         );
       } catch (error) {
         console.error("Error:", error.message);
@@ -54,7 +56,6 @@ function ShowAlarms() {
       }
     }
   };
-  
 
   const openDoctorModal = (data) => {
     setEditData(data);
@@ -85,10 +86,12 @@ function ShowAlarms() {
 
   useEffect(() => {
     const getData = async () => {
-      const result = await axiosInstance.get(`${server_url}/alarms/byPatientId/${pid}`);
+      const result = await axiosInstance.get(
+        `${server_url}/alarms/byPatientId/${pid}`
+      );
       console.log(result.data.data);
       await setUserAlarmData(result.data.data);
-      console.log(userAlarmData)
+      console.log(userAlarmData);
       setDosesData(result.data.doses);
       return result;
     };
@@ -105,44 +108,47 @@ function ShowAlarms() {
     isDoctorfunc();
   }, [showModal, showEditModal, showDoctorModal]);
 
+  const formatDate = (dateString) => {
+    if (!dateString) return "";
+    const dateObject = new Date(dateString);
+    const day = String(dateObject.getDate()).padStart(2, "0");
+    const month = String(dateObject.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+    const year = dateObject.getFullYear();
+    return `${day}-${month}-${year}`;
+  };
+
   function AlarmCard({ alarm, onDelete, onApprove, onReject }) {
-    console.log(alarm)
+    console.log(alarm);
     const alarmId = localStorage.getItem("alarmId");
     const isHighlighted = alarmId && parseInt(alarmId) === alarm.id;
-
     return (
       <tr
         key={alarm.id}
         className={`${
           isHighlighted ? "bg-green-100" : ""
-        } border-b border-gray-200`}
-      >
-        <td>{alarm.dateadded? alarm.dateadded.slice(0, 10):'-'}</td>
-        <td>{alarm.type? alarm.type.type : 'No type available'}</td>
+        } border-b border-gray-200`}>
+        <td>{formatDate(alarm.dateadded)}</td>
+        <td>{alarm.type ? alarm.type : "No type available"}</td>
 
         <td
           // align the text in center
-          className="text-center"
-        >
+          className="text-center">
           {alarm.time}
         </td>
-        <td>{alarm.timesamonth? alarm.timesamonth:'Not specified'}</td>
+        <td>{alarm.timesamonth ? alarm.timesamonth : "Not specified"}</td>
         <td>{alarm.status}</td>
-        {isDoctor &&
-        alarm.type === "Prescription"  ? (
+        {isDoctor && alarm.type === "Prescription" ? (
           <></>
         ) : (
           <td className="align-middle">
             <button
               className="text-[#87ca9c] inline-block mx-2 text-2xl"
-              onClick={() => openEditModal(alarm)}
-            >
+              onClick={() => openEditModal(alarm)}>
               <BsPencilSquare />
             </button>
             <button
               className="text-[#ff0000] inline-block mx-2 text-2xl"
-              onClick={() => deleteAlarm(alarm.id)}
-            >
+              onClick={() => deleteAlarm(alarm.id)}>
               <BsTrash />
             </button>
           </td>
@@ -163,9 +169,11 @@ function ShowAlarms() {
         <div className="container">
           <div className="bg-gray-100 min-h-screen sm:py-10 sm:px-20 ">
             <div className="w-full p-7 bg-white shadow-md border-t-4 border-primary">
-            <Link to={`/userProfile/${pid}`} className="text-primary border-b-2 border-primary">
+              <Link
+                to={`/userProfile/${pid}`}
+                className="text-primary border-b-2 border-primary">
                 go back
-                </Link>
+              </Link>
               <div className="mt-4 mb-4 flex items-center justify-end">
                 {/* <h1 className="text-2xl">Dr. {localStorage.getItem("firstname")}</h1> */}
                 {/* <img src="" alt="f" className="rounded-full h-12 w-12" /> */}
@@ -175,8 +183,7 @@ function ShowAlarms() {
                 <div className="flex items-center justify-end">
                   <button
                     className="block rounded-lg text-primary border-2 border-primary w-40 py-2"
-                    onClick={() => openModal()}
-                  >
+                    onClick={() => openModal()}>
                     Add Alarm
                   </button>
                   {showModal && (
@@ -209,7 +216,11 @@ function ShowAlarms() {
                       <th className="py-3 px-4 text-Left">Time Duration</th>
                       <th className="py-3 px-4 text-left">Monthly</th>
                       <th className="py-3 px-4 text-left">Status</th>
-                      {isDoctor ? (<></>) :(<th className="py-3 px-4 text-left">Action</th>)} 
+                      {isDoctor ? (
+                        <></>
+                      ) : (
+                        <th className="py-3 px-4 text-left">Action</th>
+                      )}
                     </tr>
                   </thead>
                   <tbody>
